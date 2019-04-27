243aa99303b9c14034f7193fcba1256b4aa2715a
raujonas <rau.jonas@web.de>
2019-04-27 09:46:49 +0200

    added time to json object

--- sendHumiTemp.py
+++ sendHumiTemp.py
@@ -3,28 +3,36 @@ import Adafruit_DHT
 import json
 import os
 import sys
+import requests
+import datetime
 
 pin = 4
 sensor = Adafruit_DHT.DHT22
 hostname = sys.argv[1]
-response = os.system("ping -c 1 " + hostname)
 
-if response == 0:
- print hostname, 'is up!'
-else:
- print hostname, 'is down!'
+#response = os.system("ping -c 1 " + hostname)
+
+#if response == 0:
+# print hostname, 'is up!'
+#else:
+# print hostname, 'is down!'
 
 while True:
   humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+  now = datetime.datetime.now().isoformat()
 
   if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+#    print('Time={2} Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity, now))
 
     data = {}
     data["temp"] = round(temperature, 2)
     data["humi"] = round(humidity, 2)
+    data["time"] = now
     json_data = json.dumps(data)
-    print "JSON: ", json_data
+    print json_data
+
+    r = requests.post('http://' + hostname + ':80/post', data=json_data)
+#    print r.content
 
     time.sleep(15)
   else:
