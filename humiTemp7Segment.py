061f1716d8326cf4b95f26f4a020800b2fff6115
Jonas Rau <rau.jonas@web.de>
2019-07-02 19:59:46 +0200

    show humi and temp on 7 segment

--- humiTemp7Segment.py
+++ humiTemp7Segment.py
@@ -6,17 +6,50 @@ import sys
 import requests
 import datetime
 
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.virtual import viewport, sevensegment
+
 pin = 4
 sensor = Adafruit_DHT.DHT22
 
-while True:
+def date(seg):
+  now = datetime.datetime.now()
+  seg.text = now.strftime("%y-%m-%d")
+
+def clock(seg):
+  interval = 0.5
+  for i in range(int(5/interval)):
+    now = datetime.datetime.now()
+    seg.text = now.strftime("%H-%M-%S")
+
+    if i % 2 == 0:
+      seg.text = now.strftime("%H-%M-%S")
+    else:
+      seg.text = now.strftime("%H %M %S")
+    time.sleep(interval)
+
+def humiTemp(seg):
   humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-  now = datetime.datetime.now().isoformat()
 
   if humidity is not None and temperature is not None:
-    print('Time={2} Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity, now))
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+#    seg.text = ('{0:0.1f} CEL'.format(temperature))
+#    time.sleep(5)
+#    seg.text = ('{0:0.1f} PER'.format(humidity))
+    seg.text = ('{0:0.1f}  {1:0.1f}'.format(temperature, humidity))
 
+def main():
+  serial = spi(port=0, device=0, gpio=noop())
+  device = max7219(serial, cascaded=1)
+  seg = sevensegment(device)
+	
+  while True:
+#    date(seg)
+#    time.sleep(3)
+#    clock(seg)
+    humiTemp(seg)
     time.sleep(15)
-  else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1) 
+
+if __name__ == '__main__':
+  main()
